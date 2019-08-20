7ad3b4f3621369f9eaf4e76c1e1a0405088a466e
minsu-zip <alstn4814@gmail.com>
2019-08-20 17:46:25 +0900

    gg

--- src/main/java/com/example/test_spring/controller/OrganizationBean.java
+++ src/main/java/com/example/test_spring/controller/OrganizationBean.java
@@ -1,18 +1,36 @@
 package com.example.test_spring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+
+@RequestMapping(value = "/organization")
 public class OrganizationBean{
-    String  ORFANIZATION_SEQ;
-    public String getORFANIZATION_SEQ(){
-        ORFANIZATION_SEQ = "<P>AAA</P>";
-        return ORFANIZATION_SEQ;
-    }
 
-    public void setORFANIZATION_SEQ(String ORFANIZATION_SEQ){
+    @RequestMapping(value = "/Input", method = RequestMethod.GET)
+    public void action1(){
 
     }
 
-    
+
+    @RequestMapping(value = "/{action}", method = RequestMethod.GET)
+    public ModelAndView action(@RequestParam Map<String, Object> paramMap, @PathVariable String action, ModelAndView modelandView){
+        String viewName = "/organization/";
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        if("read".equals(action)){
+            viewName += action;
+            resultMap = paramMap;
+        }
+        modelandView.setViewName(viewName);
+        modelandView.addObject("resultMap",resultMap);
+        return modelandView;
+    }
 }
